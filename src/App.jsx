28c80d9fc3d16684569f673de346cda310526e29
Anthony_dev <atozbasic@gmail.com>
2022-10-25 18:50:21 +0100

    added some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home1 from "./pages/homePage/Home1";
 import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route index element={<Home1 />} />
       </Routes>
       <Footer />
     </BrowserRouter>
