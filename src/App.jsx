9d6d28065bdbc39712743d67f0ee1e233e71380a
Anthony_dev <atozbasic@gmail.com>
2022-10-25 13:34:27 +0100

    config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
